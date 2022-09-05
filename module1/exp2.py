848c62244f5adf97fe14d0fe4c58850a14890324
Гасан <hasanaliyeev@gmail.com>
2022-09-05 23:40:35 +0300

    start python

--- module1/exp2.py
+++ module1/exp2.py
@@ -10,3 +10,15 @@ x = my_list[1:4]  # [56, 78, 3.14]
 y = my_list[1:]  # [56, 78, 3.14, 'text', [1, 'pen']]
 z = my_list[:]  # [True, 56, 78, 3.14, 'text', [1, 'pen']]
 g = my_list[:3]  # [True, 56, 78]
+t = my_list[-1]  # [1, 'pen']
+v = my_list[-3:-1]  # [3.14, 'text']
+
+my_new_list = [1, 'car', 7, 3.45]
+del my_new_list[1]
+nums = [2, 4, 7, 9, 1, 8, 3, 5]
+print(nums)
+del nums[1:3]
+print(nums)
+
+
+
---
+++ module1/exp3.py
@@ -0,0 +1,33 @@
+# List Methods
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+list.append(my_list, 9)
+print(my_list)  # [1, 2, 3, 4, 5, 6, 7, 9]
+
+new_list = [10, 11]
+list.extend(my_list, new_list)
+print(my_list)  # [1, 2, 3, 4, 5, 6, 7, 9, 10, 11]
+
+list.insert(my_list, 1, 0)
+print(my_list)  # [1, 0, 2, 3, 4, 5, 6, 7, 9, 10, 11]
+
+list.remove(my_list, 4)
+print(my_list)  # [1, 0, 2, 3, 5, 6, 7, 9, 10, 11]
+
+list.pop(my_list, 1)
+print(my_list)  # [1, 2, 3, 5, 6, 7, 9, 10, 11]
+
+nums = [7, 6, 2, 5, 7, 6, 3, 7]
+x = list.count(nums, 7)
+print(x)  # 3
+
+print(list.index(nums, 7))  # 0
+
+y = list.sort(nums)
+print(nums)  # [2, 3, 5, 6, 6, 7, 7, 7]
+
+a = [1, 6, 3]
+a.reverse()
+print(a)
+
+
